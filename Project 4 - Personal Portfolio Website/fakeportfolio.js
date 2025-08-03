ea30df1d88c8bdb528158782cbda454939706246
Aryan Kumar <aryanthakur9761178796@gmail.com>
2025-08-03 20:27:14 +0530

    Delete Project 4 - Personal Portfolio Website/fakeportfolio.js

--- Project 4 - Personal Portfolio Website/fakeportfolio.js
+++
@@ -1,9 +0,0 @@
-const body = document.querySelector('body');
-const header = document.querySelector('header');
-const menuOpen = document.querySelector('.fa-bars-staggered');
-const navbarCollapse = document.querySelector('.navbar-collapse');
-
-menuOpen.onclick = () => {
-    navbarCollapse.classList.add('show');
-    menuOpen.classList.toggle('fa-xmark');
-}
